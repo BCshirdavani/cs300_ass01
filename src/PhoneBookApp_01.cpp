3fbf4e7a7a8200527d29e3e9ef85bea25c46ba33
Beau Shirdavani <beau.shirdavani@bellevuecollge.edu>
2017-10-04 20:35:39 -0700

    manually testing the file input to read the phonebook.txt, all edits in main method.

--- src/PhoneBookApp_01.cpp
+++ src/PhoneBookApp_01.cpp
@@ -8,6 +8,9 @@
 //============================================================================
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include "Contact.h"
 using namespace std;
 
@@ -18,6 +21,33 @@ int main() {
 	// MANUAL test of Contact class
 	Contact cont1("Jason Statham", 12345);
 	cout << "test overloaded stream op. Contact class: " << cont1 << endl;
+
+	// MANUAL test of read file INPUT
+	// read lines manually put text into array of strings
+	// each row gets string array element
+	cout << "About to test the file import to string array" << endl;
+	ifstream infile("phonebook.txt");
+	if (!infile)
+	{
+		cout << "cannot find phonebook.txt" << endl;
+	}
+	string strARRAY[151430];
+	string first;
+	string last;
+	string num;
+	int count = 0;
+	for (int i =0; i < 10; i++)
+	{
+		getline(infile, first, ' ');
+		getline(infile, last, ' ');
+		getline(infile, num);
+		cout << "first: " << first << " last: " << last << " num: " << num;
+	}
+
+	infile.close();
+
+
+	cout << "end from Eclipse edit..." << endl;
 
 	return 0;
 }
